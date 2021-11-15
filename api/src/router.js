fdefe3b11bec71fa614db3aa6fd46083ea8bc1ae
Marco <guzmanescobar3@gmail.com>
2021-11-16 00:01:11 +0100

    encore un

--- api/src/router.js
+++ api/src/router.js
@@ -22,7 +22,7 @@ const filename = (request, file, callback) => {
 const fileFilter = (request, file, callback) => {
     if (file.mimetype !== "image/png") {
         request.fileValidationError = "Wrong file type"
-        callback(null, false, {Error: "Wrong file type"})
+        callback(null, false, new Error = "Wrong file type")
     } else {
         callback(null, true) 
     }
@@ -34,9 +34,7 @@ const upload = multer({
 })
 
 router.post('upload', upload.single('photo'), (request, response) => {
-    if (request.fileValidationError) {
-        return response.status(400).json({error: request.fileValidationError})
-    } 
+    if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError})
     response.status(201).json({success: true})
 })
 module.exports = router
