6f5551ee0ddcd64010fd2ce785d4271112b2cb8d
Marco <guzmanescobar3@gmail.com>
2021-11-16 11:39:53 +0100

    encore un

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,16 @@
-const Router = require('express')
-const router = Router()
+const {Router} = require('express')
 const multer = require('multer')
+const path = require('path');
+const imageProcessor = require('./imageProcessor');
+
+const router = Router()
+
+
+//Let's write a function called filename. It should take request, file, and callback as its parameters.
+const filename = (request, file, callback) => {
+    //Inside the function body make a call to callback() passing in null as the first argument and file.originalname as the second argument.
+    callback(null, file.originalname)
+}
 
 /* 
 Pass an object literal as the only argument.
@@ -9,20 +19,13 @@ Pass an object literal as the only argument.
 */
 const storage = multer.diskStorage({
     destination: "api/uploads/", 
-    filename: "filename"
+    filename
 })
-
-//Let's write a function called filename. It should take request, file, and callback as its parameters.
-const filename = (request, file, callback) => {
-
-    //Inside the function body make a call to callback() passing in null as the first argument and file.originalname as the second argument.
-    callback(null, file.originalname)
-}
 
 const fileFilter = (request, file, callback) => {
     if (file.mimetype !== "image/png") {
         request.fileValidationError = "Wrong file type"
-        callback(null, false, new Error = "Wrong file type")
+        callback(null, false, new Error("Wrong file type"))
     } else {
         callback(null, true) 
     }
@@ -35,6 +38,7 @@ const upload = multer({
 
 router.post('upload', upload.single('photo'), (request, response) => {
     if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError})
-    response.status(201).json({success: true})
+    
+    return response.status(201).json({success: true})
 })
 module.exports = router
